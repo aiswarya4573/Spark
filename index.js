2e22d7e3b375adcaa01b466ca7d1a3ade9397fb8
aiswarya4573 <156643223+aiswarya4573@users.noreply.github.com>
2024-05-08 15:15:48 +0530

    final2

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 //document.querySelector("button").setAttribute("href","login.html");
 window.open('login.html','_blank');
 window.open('enter.html','_blank');
+window.addEventListener('popstate',function(event){
+    window.history.back();
+
+});
